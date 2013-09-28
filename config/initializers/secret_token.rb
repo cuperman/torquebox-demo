8e0b07ae2e223e3c0d91b3fb0c70db14c2fa3854
Jeff Cooper <jeff@kickstorming.com>
2013-09-28 07:50:31 -0700

    created rails 3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TorqueboxDemo::Application.config.secret_token = 'e239b77f78a0e0ad60a73b78c6c8de862470b6bca203cf4eb30f5d3a2ff9f5fe7108cb741f5aa479d3f1ff9066884443a7dc0cb6a90a20b11dba9fcb006683f3'
